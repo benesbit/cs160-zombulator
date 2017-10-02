0d76af875b359961a0f664ed0597d6d7b507ce75
xDOASx <nesbitb@oregonstate.edu>
2017-10-02 09:49:51 -0700

    Draw 2 simple circles

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 // Zombulator by Ben Nesbit
 
 function setup() {
-
+	createCanvas(800, 800);
 }
 
 function draw() {
+	fill(255, 0, 0);
 	ellipse(50, 50, 80, 80);
+	fill(150, 150, 200);
+	ellipse(200, 100, 80, 80);
 }
 
