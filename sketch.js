4e2cb057d29941bf638c7710de0d80d0b727806d
xDOASx <benesbit@yahoo.com>
2017-10-16 09:30:00 -0700

    Human and Zombie

--- sketch.js
+++ sketch.js
@@ -436,6 +436,7 @@ function draw(){
 // independentally of eachother. Do not need to change the x-position
 // (which) is unfortunate because this is the easy part!
 
+/*
 var zombieY, zombieX, zombieV, zombieA, zombieDamping, zombieSize, zombieColor, backgroundColor;
 var humanY, humanV, humanA, humanDamping, humanSize, humanColor, humanColor;
 var humanX, humanXV, humanXA, humanSw;
@@ -449,7 +450,7 @@ function preload() {
 		image(img, 0, 0);
 	});
 }*/
-
+/*
 function setup() {
 	createCanvas(720, 400);
 
@@ -567,4 +568,62 @@ function moveHuman(){
    		// humanSw = 0;
 	}
 	//}
+}*/
+
+
+// Zombulator by Ben and Hannah
+// CS 160 Exercise 12: Function practice. Preamble to arrays.
+
+var backgroundColor;
+
+const MIN_SIZE = 25; // old browser? change to var.
+const MAX_SIZE = 500;
+
+var zombieX;
+var zombieY;
+var zombieSize;
+var zombieColor;
+
+var humanX;
+var humanY;
+var humanSize;
+var humanColor;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  backgroundColor = color(245, 255, 245);
+  initializeZombie();
+  initializeHuman();
+}
+
+function draw() {
+  background(backgroundColor);
+  noStroke();
+
+  drawZombie();
+  drawHuman();
+}
+
+function initializeZombie() {
+  zombieX = random(0, windowWidth);
+  zombieY = random(0, 200);
+  zombieSize = random(MIN_SIZE, MAX_SIZE);
+  zombieColor = color(random(50, 255), random(50, 255), random(50, 255), 150);
+}
+
+function initializeHuman() {
+  humanX = random(0, windowWidth);
+  humanY = random(windowHeight - 200, windowHeight);
+  humanSize = random(MIN_SIZE, MAX_SIZE);
+  humanColor color(random(256), random(256), random(256), 150);
+}
+
+function drawZombie() {
+  fill(zombieColor);
+  ellipse(zombieX, zombieY, zombieSize, zombieSize);
+}
+
+function drawHuman() {
+  fill(humanColor);
+  ellipse(humanX, humanY, humanSize, humanSize);
 }
