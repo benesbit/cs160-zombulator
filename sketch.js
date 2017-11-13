ee91739e489a88cd30c47091aa2ab8c143fbaac9
xDOASx <benesbit@yahoo.com>
2017-11-13 11:58:11 -0800

    Removed unused consts

--- sketch.js
+++ sketch.js
@@ -6,10 +6,6 @@ var backgroundColor;
 const MIN_SIZE = 10; 
 const MAX_SIZE = 25;
 const MAX_POPULATION = 100;
-// const LOWER_ZOMBIE_POP_BOUND = .35;
-// const UPPER_ZOMBIE_POP_BOUND = .6;
-// Using these to create a percentage change based off max population. These are passed to
-// the random function so we get between a 35% and 60% ratio, randomly.
 
 const HUMAN_SPAWN_BOUND = 150;
 const ZOMBIE_SPAWN_BOUND = 150;
